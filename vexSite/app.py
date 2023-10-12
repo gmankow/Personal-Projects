93e8463e9eee5bb7cc1cf2b5d6c51fa393bd277a
Gabriel Mankowski <gmankow@iastate.edu>
2023-10-12 16:43:43 -0500

    basic html page

--- vexSite/app.py
+++ vexSite/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Test Home</p>"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
