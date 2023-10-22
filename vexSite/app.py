c8cda7d14d9434227299a69608554b0ee2bf7f6c
Gabriel Mankowski <gmankow@iastate.edu>
2023-10-22 09:23:10 -0500

    Co-authored-by: Page, Jacob C <jcpage@iastate.edu> Co-authored-by: MagicSpronk <MagicSpronk@users.noreply.github.com>

--- vexSite/app.py
+++ vexSite/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import socket
 
+hostname = socket.gethostname()
+IP = socket.gethostbyname(hostname)
 
 app = Flask(__name__)
 
@@ -7,5 +10,11 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/Our-Team")
+def bios():
+    return render_template("bios.html")
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(host=IP, debug=True, port=8000)
